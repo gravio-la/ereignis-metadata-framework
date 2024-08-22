6f9342e9225bb6161a68b7ec299bb1ee1db53e9e
Sebastian Tilsch <winzlieb@c3d2.de>
2024-08-22 14:43:37 +0200

    add story and cleanup basic-components

--- packages/basic-components/src/dialog/index.ts
+++ packages/basic-components/src/dialog/index.ts
@@ -1,1 +1,2 @@
 export * from "./MuiEditDialog";
+export * from "./GenericModal";
--- packages/basic-components/src/index.tsx
+++ packages/basic-components/src/index.tsx
@@ -1,4 +1,3 @@
-export * from "./GenericModal";
 export * from "./list";
 export * from "./entity";
 export * from "./overflow";
