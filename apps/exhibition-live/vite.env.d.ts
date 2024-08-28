bb7e8e61bf23dd82b6aba8be3ae1ba53d7a81887
Sebastian Tilsch <winzlieb@c3d2.de>
2024-08-28 14:17:06 +0200

    add emv variable to vite to specify the current MANIFESTATION

--- apps/exhibition-live/vite.env.d.ts
+++ apps/exhibition-live/vite.env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_SPARQL_ENDPOINT_LABEL: string;
   readonly VITE_SPARQL_ENDPOINT_PROVIDER: string;
   readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_APP_MANIFESTATION: "exhibition" | "kulinarik";
   // more env variables...
 }
 
