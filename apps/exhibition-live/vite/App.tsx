a6da4df3358ca97debc1e1f35fe01bd0383787ca
Winzlieb <winzlieb@c3d2.de>
2025-02-19 03:31:00 +0100

    feat: add OxigraphProvider and OxigraphInit components for loading RDF data sources

--- apps/exhibition-live/vite/App.tsx
+++ apps/exhibition-live/vite/App.tsx
@@ -1,4 +1,9 @@
-import { AdbProvider, store } from "@graviola/edb-state-hooks";
+import {
+  AdbProvider,
+  store,
+  useOxigraph,
+  useRDFDataSources,
+} from "@graviola/edb-state-hooks";
 import { Provider } from "react-redux";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import "dayjs/locale/de";
@@ -21,11 +26,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeComponent } from "@graviola/edb-default-theme";
 
 import "react-json-view-lite/dist/index.css";
+import "@triply/yasgui/build/yasgui.min.css";
 import { availableAuthorityMappings } from "@slub/exhibition-schema";
+import { Box, CircularProgress } from "@mui/material";
 
 export const queryClient = new QueryClient();
 
 const sparqlEndpoint = envToSparqlEndpoint(import.meta.env, "VITE");
+console.log("sparqlEndpoint", import.meta.env);
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const appConfig =
@@ -33,9 +41,48 @@ const appConfig =
     ? kulinarikAppConfig
     : exhibitionConfig;
 
+const Loading = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+
+const OxigraphProvider = ({ children }: { children?: React.ReactNode }) => {
+  const { oxigraph } = useOxigraph();
+  if (!oxigraph?.ao) {
+    return <Loading />;
+  }
+
+  return <>{children}</>;
+};
+
+const OxigraphInit = ({ children }: { children?: React.ReactNode }) => {
+  const { bulkLoaded } = useRDFDataSources(
+    "/ontology/exhibition-info.owl.ttl",
+    BASE_IRI,
+  );
+  const { bulkLoaded: bulkLoaded2 } = useRDFDataSources(
+    "/example-exhibitions.ttl",
+    BASE_IRI,
+  );
+  if (!bulkLoaded || !bulkLoaded2) {
+    return <Loading />;
+  }
+  return <>{children}</>;
+};
+
 export const App = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient} contextSharing={true}>
+    <QueryClientProvider client={queryClient}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <Provider store={store}>
           <ThemeComponent>
@@ -57,7 +104,11 @@ export const App = ({ children }: { children?: React.ReactNode }) => {
                 normDataMapping={availableAuthorityMappings}
               >
                 <GoogleOAuthProvider clientId={googleClientId}>
-                  <NiceModal.Provider>{children}</NiceModal.Provider>
+                  <OxigraphProvider>
+                    <OxigraphInit>
+                      <NiceModal.Provider>{children}</NiceModal.Provider>
+                    </OxigraphInit>
+                  </OxigraphProvider>
                 </GoogleOAuthProvider>
               </AdbProvider>
             </SnackbarProvider>
