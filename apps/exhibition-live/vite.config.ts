8ae9fda4ef4a589a7a301e08664f30648eebebae
Winzlieb <winzlieb@c3d2.de>
2025-02-20 16:04:21 +0100

    feat: add Vite build script and rollup-plugin-visualizer dependency for enhanced build process

--- apps/exhibition-live/vite.config.ts
+++ apps/exhibition-live/vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      emitFile: true,
+      filename: "stats.json",
+      template: "raw-data",
+    }) as PluginOption,
+  ],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
