df01f4cdc1e8562cb020f628ecccbb1380c83ce2
Winzlieb <winzlieb@c3d2.de>
2025-02-24 00:46:49 +0100

    feat: update ESLint configuration with new rules and add eslint-import-resolver-alias dependency

--- packages/eslint-config-edb/index.js
+++ packages/eslint-config-edb/index.js
@@ -4,5 +4,12 @@ module.exports = {
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "turbo/no-undeclared-env-vars": "off",
+    "simple-import-sort/imports": "error",
+    semi: ["error", "always"],
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-debugger": "error",
+    "no-alert": "error",
+    "no-unused-expressions": "error",
+    "eslint-import-resolver-alias": "error",
   },
 };
