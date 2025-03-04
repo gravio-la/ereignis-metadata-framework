60aa77ef6c7a3ffaa22a902b58baeacd5634af56
Winzlieb <winzlieb@c3d2.de>
2025-03-04 13:56:51 +0100

    feat: enhance clean-package config to replace workspace references in dependencies

--- clean-package.config.cjs
+++ clean-package.config.cjs
@@ -1,3 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+
+
+const packageFiles = glob.sync(path.join(__dirname, 'packages/**/package.json'), {
+  ignore: ['**/node_modules/**'],
+  absolute: true
+});
+
+const packageVersions = new Map();
+
+packageFiles.forEach(packagePath => {
+  try {
+    const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+    if (packageJson.name && packageJson.version) {
+      packageVersions.set(packageJson.name, packageJson.version);
+    }
+  } catch (error) {
+    console.error(`Error processing ${packagePath}:`, error);
+  }
+});
+
+const replaceWorkspaceReferences = (dependencies) => {
+  if (!dependencies) return dependencies;
+  
+  const result = { ...dependencies };
+  
+  for (const [dep, version] of Object.entries(result)) {
+    if (version === 'workspace:*' && packageVersions.has(dep)) {
+      result[dep] = `^${packageVersions.get(dep)}`;
+    }
+  }
+  
+  return result;
+};
+
+
+
 module.exports = {
   remove: [
     "scripts",
@@ -9,4 +48,14 @@ module.exports = {
     "eslintConfig",
     "typedocOptions",
   ],
+  onClean: () => {
+    const pckg = JSON.parse(fs.readFileSync('./package.json'));
+    if(pckg.peerDependencies) {
+      pckg.peerDependencies = replaceWorkspaceReferences(pckg.peerDependencies);
+    }
+    if(pckg.dependencies) {
+      pckg.dependencies = replaceWorkspaceReferences(pckg.dependencies);
+    }
+    fs.writeFileSync('./package.json', JSON.stringify(pckg, null, 2));
+  }
 };
